d24dc8f86412ba418d915b15761423996537db5c
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-11-15 11:43:53 +0100

    init

---
+++ javascript.js
@@ -0,0 +1,40 @@
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+  }
+  
+  
+  function setCookie(cname,cvalue,exsekunder) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exsekunder*1*1000000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function deleteCookie(cname,cvalue,exsekunder) {
+      document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
+  
